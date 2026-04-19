9b97e39bbf22d99d07b16f9d9c111d4f762a256e
juliana15paak <fvjulianaasilva976@gmail.com>
2026-04-19 13:26:01 -0300

    feat: otimiza script js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const btnMenu = document.querySelector('.menu');
-const menuLista = document.querySelector('nav ul');
-
-if(btnMenu && menuLista){
-  btnMenu.addEventListener('click', () =>{
-    menuLista.classList.toggle('menu-aberto');
-  })
-}
